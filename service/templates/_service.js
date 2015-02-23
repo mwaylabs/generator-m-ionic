1a137399b6ce2a3c2ecd4b4da3c76f7458e9a668
Jonathan Grupp <j.grupp@mwaysolutions.com>
2015-02-23 11:33:08 +0100

    minor template updates

--- service/templates/_service.js
+++ service/templates/_service.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('<%= moduleName %>')
 .service('<%= serviceName %>', function () {
-  console.log('Hello from your Service: <%= name %> in module <%= moduleName %>');
+  console.log('Hello from your Service: <%= serviceName %> in module <%= moduleName %>');
 <% if(options.sample === 'start') {%>
   // some initial data
   this.someData = {
