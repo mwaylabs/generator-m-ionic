de8961213d78e8b2f52b823b08280bb9205e6b96
DrMabuse23 <drmabuse@posteo.de>
2015-03-15 10:42:48 +0100

    add ngdoc for service

--- service/templates/_service.js
+++ service/templates/_service.js
@@ -1,9 +1,19 @@
 'use strict';
+/**
+* @ngdoc service
+* @name <%= moduleName %>:<%= serviceName %>
+* @description add your description
+*/
 angular.module('<%= moduleName %>')
-.service('<%= serviceName %>', function () {
+.service('<%= serviceName %>', function <%= serviceName %>() {
   console.log('Hello from your Service: <%= serviceName %> in module <%= moduleName %>');
 <% if(options.sample === 'start') {%>
-  // some initial data
+  /**
+  * @ngdoc method
+  * @name someData
+  * @description some initial data
+  * @methodOf <%= moduleName %>:<%= serviceName %>
+  */
   this.someData = {
     binding: 'Yes! Got that databinding working'
   };
