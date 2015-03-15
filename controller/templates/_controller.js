3f031901d7af67c94580f4bf3c8d3a181bae49bc
DrMabuse23 <drmabuse@posteo.de>
2015-03-15 10:42:16 +0100

    add ng doc to controller

--- controller/templates/_controller.js
+++ controller/templates/_controller.js
@@ -1,10 +1,27 @@
 'use strict';
+/**
+* @ngdoc controller
+* @name <%= moduleName %>:<%= controllerName %>
+* @requires $scope
+* @description add your description
+*/
 angular.module('<%= moduleName %>')
-.controller('<%= controllerName %>', function ($scope<% if(options.sample === 'start') { %>, Start, Config<% } %>) {
+.controller('<%= controllerName %>', function <%= controllerName %>($scope<% if(options.sample === 'start') { %>, Start, Config<% } %>) {
   console.log('Hello from your Controller: <%= controllerName %> in module <%= moduleName%>:. This is your scope:', $scope);
 <% if (options.sample === 'start') {%>
-  // bind data from service
+  /**
+   * @ngdoc property
+   * @name .#someData
+   * @propertyOf <%= moduleName %>:<%= controllerName %>
+   * @returns {string}
+   */
   $scope.someData = Start.someData;
+  /**
+   * @ngdoc property
+   * @name .#env
+   * @propertyOf <%= moduleName %>:<%= controllerName %>
+   * @returns {string}
+   */
   $scope.env = Config.ENV;
 <% } %>
   // TODO: do your controller thing
