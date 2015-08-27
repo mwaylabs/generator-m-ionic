0ce9cecfb3344af581e7e6f414156191db6f660d
gruppjo <jona_blubb@web.de>
2015-08-27 17:09:35 +0200

    Revert "closes #225: inject location of default modules"
    This reverts commit 2f5dba59271b5bf43c7045e1031d7f37a5946d71.

--- app/templates/_app.js
+++ app/templates/_app.js
@@ -1,9 +1,5 @@
 'use strict';
 angular.module('<%= answers.appModule %>', [
-  'ionic',
-  'ngCordova',
-  'ui.router',
-
-  // load your own modules here
+  // load your modules here
   'main', // starting with the main module
 ]);
--- module/templates/_module.js
+++ module/templates/_module.js
@@ -1,5 +1,10 @@
 'use strict';
-angular.module('<%= moduleName %>', [])
+angular.module('<%= moduleName %>', [
+  'ionic',
+  'ngCordova',
+  'ui.router',
+  // TODO: load other modules selected during generation
+])
 .config(function ($stateProvider<% if (options.mainModule) {%>, $urlRouterProvider<%} %>) {
 
   // ROUTING with ui.router
