650aca0cd22f33bc7b26dffe73dd4bf3ee6fe169
gruppjo <jona_blubb@web.de>
2015-09-02 18:34:08 +0200

    work on #213: better jenkins.sh

--- app/templates/jenkins.sh
+++ app/templates/jenkins.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# TODO: clone repository
+# clone repository
+git clone https://github.com/mwaylabs/generator-m-demo
+
+# change into directory
+cd generator-m-demo
 
 # install dependencies
 npm install
 bower install
-gulp cordova-install
 
-# gulp build
-gulp build
+# TODO: set BUILD variable
+BUILD=BUILD
 
-# TODO: cordova builds etc...
+# CONFIGURATION
+# retrieve version from config.xml
+VERSION="$(gulp config --silent --getWidgetAttr=version)"
+# append the build number and save in config.xml
+gulp config --setWidgetAttr="version=${VERSION}.${BUILD}"
+# set the android-versionCode
+gulp config --setWidgetAttr="android-versionCode=${BUILD}"
+
+# BUILDING example
+# gulp build: run separately to make it more obvious what's happening
+# build: inject version and build number into app
+# build:  with dev environment und resources set1
+# build: minify
+gulp build --buildVars="version:${VERSION},build:${BUILD}" --env=dev --res=set1 --minify
+# cordova build
+gulp --cordova 'build ios'
+
