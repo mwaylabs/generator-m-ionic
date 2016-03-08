c80ac2ccb3c17d3ffcb4e1842d8a35772df77f21
gruppjo <jona_blubb@web.de>
2016-03-08 12:42:26 +0100

    fixes #333: update components, bower, npm

--- generators/app/sources/bower-config.js
+++ generators/app/sources/bower-config.js
@@ -6,20 +6,20 @@ module.exports = {
   bowerJSON: {
     dependencies: {
       'ionic': '~1.2.0',
-      'angular': '~1.4.5',
-      'angular-animate': '~1.4.5',
-      'angular-sanitize': '~1.4.5',
+      'angular': '~1.5.0',
+      'angular-animate': '~1.5.0',
+      'angular-sanitize': '~1.5.0',
       'angular-ui-router': '~0.2.15',
       'ngCordova': '~0.1.17-alpha'
     },
     devDependencies: {
-      'angular-mocks': '~1.4.5'
+      'angular-mocks': '~1.5.0'
     },
     resolutions: {
       // force newest angular versions (not those from ionic)
-      'angular': '~1.4.5',
-      'angular-animate': '~1.4.5',
-      'angular-sanitize': '~1.4.5',
+      'angular': '~1.5.0',
+      'angular-animate': '~1.5.0',
+      'angular-sanitize': '~1.5.0',
       'angular-ui-router': '~0.2.15'
     }
   },
@@ -31,18 +31,18 @@ module.exports = {
       checked: true
     },
     {
-      value: 'angular-translate#~2.8.1',
+      value: 'angular-translate#~2.10.0',
       name: 'angular-translate',
       checked: true
     },
     {
-      value: 'angular-translate-loader-static-files#~2.8.1',
+      value: 'angular-translate-loader-static-files#~2.10.0',
       name: 'angular-translate-loader-static-files',
       checked: true
     },
     new inquirer.Separator('---- persistence ----'),
     {
-      value: 'localforage#~1.3.1',
+      value: 'localforage#~1.4.0',
       name: 'localforage',
       checked: true
     }
